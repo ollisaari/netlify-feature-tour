efbb45790db3e7434a9bd0bf5ac9a6330bdb2ba2
Olli Saari <olli.saari@arttia.fi>
2023-10-10 21:05:52 +0300

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://feature-tour.netlify.app',
+  site: 'https://arttia.netlify.app',
   integrations: [
     preact(),
     sitemap({
-      canonicalURL: 'https://feature-tour.netlify.app'
+      canonicalURL: 'https://arttia.netlify.app'
     })
   ],
 });
