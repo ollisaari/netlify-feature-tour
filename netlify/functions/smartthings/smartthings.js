64f44e4c0d58cea5a98cb234de516ab20c37a80a
Olli Saari <olli.saari@arttia.fi>
2023-10-10 22:40:43 +0300

    fix: Add CORS headers to response

--- netlify/functions/smartthings/smartthings.js
+++ netlify/functions/smartthings/smartthings.js
@@ -105,6 +105,10 @@ exports.handler = async (event, context) => {
 
             return {
                 statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "Content-Type"
+                },
                 body: JSON.stringify(`Temperature set to ${desiredTemperature} degrees (ero: ${difference}).`)
             };
 
